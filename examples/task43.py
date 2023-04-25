016f065749b3be6feff7be2462f6c25bda2c4134
sergey <sergey@solaev.ru>
2023-04-25 13:08:46 +0400

    Add homework

--- examples/task43.py
+++ examples/task43.py
@@ -12,3 +12,17 @@ num_list = [1, 2, 1, 3, 4, 5, 3, 4, 1]
 pair_count = sum(num_list.count(i)//2 for i in set(num_list))
 
 print(pair_count)
+
+# def count_ps():
+# arr = input("input: ").split()
+# arr = list(map(int, arr))
+# count = 0
+# while len(arr) > 1:
+# x = arr.pop()
+# # print(arr)
+# if x in arr:
+# count +=1
+# arr.remove(x)
+# print(arr)
+# return count
+# print(count_ps())
